7ce98d1353cd96da84b0983635ca1764387b4669
mrsfdri <mohammadrezaasafdari@gmail.com>
2018-04-01 02:18:28 +0430

    Create project v1.0

---
+++ CodeVision/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Created: 4/1/2018 2:04:40 AM
+ * Author: mrsfdri
+ */
+
+#include <io.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
